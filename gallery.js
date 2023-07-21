068df43f788902f30bb5b60ff45d533dd1931b1a
simarkaur2003 <112979314+simarkaur2003@users.noreply.github.com>
2023-07-21 03:09:56 -0400

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -1,78 +1,93 @@
-// Array of gallery items
-const galleryItems = [
-    {
-      filename: 'flowers-pink-large.jpg',
-      alt: 'Pink Flowers',
-      info: 'Pink flowers in a garden.',
-    },
-    {
-      filename: 'flowers-purple-large.jpg',
-      alt: 'Purple Flowers',
-      info: 'Purple flowers blooming in a field.',
-    },
-    {
-      filename: 'flowers-red-large.jpg',
-      alt: 'Red Flowers',
-      info: 'Vibrant red flowers in a bouquet.',
-    },
-    {
-      filename: 'flowers-white-large.jpg',
-      alt: 'White Flowers',
-      info: 'Elegant white flowers against a green backdrop.',
-    },
-    {
-      filename: 'flowers-yellow-large.jpg',
-      alt: 'Yellow Flowers',
-      info: 'Bright yellow flowers in a sunny garden.',
-    },
+// Attaching event listeners to trigger functions once the page finishes loading
+document.addEventListener('DOMContentLoaded', function () {
+
+  // Declaring constants to prevent repetitive image element retrieval
+  const displayImage = document.getElementById('current-image');
+  const imageTitle = document.getElementById('image-title');
+  const thumbnailsList = document.getElementById('thumbnails-list');
+  
+  const imageCollection = [
+  {
+  file: 'flowers-yellow',
+  caption: 'Sunflowers in the hamlet Dernekamp, Kirchspiel, Dülmen, North Rhine-Westphalia, Germany',
+  },
+  {
+  file: 'flowers-red',
+  caption: 'Poppies in cornfield, Dülmen, North Rhine-Westphalia, Germany',
+  },
+  {
+  file: 'flowers-white',
+  caption: 'Daffodils in Sentmaring park, Münster, North Rhine-Westfalia, Germany',
+  },
+  {
+  file: 'flowers-purple',
+  caption: 'Sentmaring Park, Münster, North Rhine-Westphalia, Germany',
+  },
+  {
+  file: 'flowers-pink',
+  caption: 'Market in Münster, North Rhine-Westphalia, Germany',
+  },
   ];
   
-  // Get the thumbnail container element
-  const thumbnailContainer = document.querySelector('ul');
+  // Initializing the current image index as zero
+  let currentImageIndex = 0;
   
-  // Get the main image figure and figcaption elements
-  const mainImageFigure = document.querySelector('.main-image');
-  const mainImageCaption = document.querySelector('.main-image figcaption');
+  // Function to set the current image to be displayed
+  function setCurrentImage(image) {
+  displayImage.src = `images/${image.file}-large.jpg`;
+  imageTitle.textContent = image.caption;
+  }
   
-  // Build the thumbnail list dynamically
-  galleryItems.forEach(function (item, index) {
-    // Create a new list item element
-    const listItem = document.createElement('li');
+  // Function to add thumbnail images to the list for display
+  function addThumbnails() {
+  for (let i = 0; i < imageCollection.length; i++) {
+  const image = imageCollection[i];
+  const listItem = document.createElement('li');
+  const thumbnail = document.createElement('img');
+ 
+    thumbnail.src = `images/${image.file}-small.jpg`;
+    thumbnail.alt = image.caption;
+    thumbnail.width = 240;
+    thumbnail.height = 160;
   
-    // Create a new image element
-    const imageElement = document.createElement('img');
-    imageElement.src = 'images/' + item.filename;
-    imageElement.alt = item.alt;
+    // Setting the active and inactive classes for the thumbnail images
+    if (i === currentImageIndex) {
+      thumbnail.classList.add('active');
+    } else {
+      thumbnail.classList.add('inactive');
+    }
   
-    // Create a new figcaption element for the additional information
-    const figcaption = document.createElement('figcaption');
-    figcaption.textContent = item.info;
-  
-    // Append the image element and figcaption to the list item
-    listItem.appendChild(imageElement);
-    listItem.appendChild(figcaption);
-  
-    // Add click event listener to the list item
-    listItem.addEventListener('click', function () {
-      // Update the main image with the clicked thumbnail
-      const mainImage = document.querySelector('.main-image img');
-      mainImage.src = imageElement.src;
-      mainImage.alt = imageElement.alt;
-  
-      // Update the caption with the alt attribute of the thumbnail
-      mainImageCaption.textContent = item.info;
-  
-      // Remove the 'selected' class from all thumbnails
-      const thumbnails = document.querySelectorAll('ul li');
-      thumbnails.forEach(function (thumbnail) {
-        thumbnail.classList.remove('selected');
-      });
-  
-      // Add the 'selected' class to the clicked thumbnail
-      listItem.classList.add('selected');
+    thumbnail.addEventListener('click', function () {
+      currentImageIndex = i;
+      setCurrentImage(image);
+      updateThumbnails();
     });
   
-    // Append the list item to the thumbnail container
-    thumbnailContainer.appendChild(listItem);
-  });
+    listItem.appendChild(thumbnail);
+    thumbnailsList.appendChild(listItem);
+  }
+  }
   
+  // Function to update the active and inactive status of thumbnails
+  function updateThumbnails() {
+  const thumbnails = thumbnailsList.getElementsByTagName('img');
+  for (let i = 0; i < thumbnails.length; i++) {
+  if (i === currentImageIndex) {
+  thumbnails[i].classList.add('active');
+  thumbnails[i].classList.remove('inactive');
+  } else {
+  thumbnails[i].classList.add('inactive');
+  thumbnails[i].classList.remove('active');
+  }
+  }
+  }
+  
+  // Function to initialize the gallery by setting the current image and adding thumbnails
+  function initializeGallery() {
+  setCurrentImage(imageCollection[currentImageIndex]);
+  addThumbnails();
+  }
+  
+  // Calling the initializeGallery function when the DOM is loaded
+  initializeGallery();
+  });
